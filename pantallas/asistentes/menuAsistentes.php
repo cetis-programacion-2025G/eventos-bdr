281f65e9ae88ab93c6ba68552c1e0e75978ac883
Angel Haziel <angelhaziel876@gmail.com>
2026-06-10 23:23:56 -0600

    actualiza sistema

--- pantallas/asistentes/menuAsistentes.php
+++ pantallas/asistentes/menuAsistentes.php
@@ -1,27 +1,37 @@
 <?php
-function menuAsistentes(&$datos) {
+
+function menuAsistentes($conn) {
+
     $salir = false;
+
     while (!$salir) {
+
         limpiarPantalla();
+
         echo "\n";
         titulo("ASISTENTES", 94);
+
         echo str_repeat("─", 96) . "\n";
+
         echo " 1. Ver asistentes\n";
         echo " 2. Registrar asistente\n";
         echo " 0. Regresar\n";
+
         echo str_repeat("─", 96) . "\n";
 
         $op = pedirEntero("Opcion", [0, 1, 2]);
+
         switch ($op) {
+
             case 1:
-                limpiarPantalla();
-                listarAsistentes($datos);
+                listarAsistentes($conn);
                 esperarEnter();
                 break;
+
             case 2:
-                limpiarPantalla();
-                registrarAsistente($datos);
+                registrarAsistente($conn);
                 break;
+
             case 0:
                 $salir = true;
                 break;
